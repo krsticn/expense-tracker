6ed1de1b8a40c534c58bb8559708694f6433a60d
Krsticn <krsticn94@gmail.com>
2022-05-12 13:00:12 +0200

    Map function is added to expenses list to dynamicly render ExpenseItem components with NewExpensesdata.State is added to App Component and is passed to expenses list

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
+import React, { useState } from "react";
 import ExpensesList from "./components/Expenses/ExpensesList";
 import NewExpenses from "./components/NewExpenses/NewExpenses";
 
+const DUMMY_DATA = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenseData, setExpenseData] = useState(DUMMY_DATA);
 
   const addExpenseHandler = (newExpenseData) => {
-    console.log(newExpenseData);
-    console.log("Logged in App.js");
+    setExpenseData((previousExpense) => {
+      return [newExpenseData, ...previousExpense];
+    });
   };
 
   return (
     <div>
       <NewExpenses onAddExpense={addExpenseHandler} />
-      <ExpensesList items={expenses} />
+      <ExpensesList items={expenseData} />
     </div>
   );
 };
